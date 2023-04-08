6c2dcc3a204ef06882440181ffff6e57f5356c18
SileboxUnderfined <valerkafm@gmail.com>
2023-04-08 16:29:47 +0300

    update

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from pyrogram import Client
 from configparser import ConfigParser
+from resource_path import resource_path
 
 config: ConfigParser = ConfigParser()
-config.read('config.ini')
+config.read(resource_path('config.ini'))
 info = config['Info']
 
 session_name: str = input('Введите название для файла: ')
@@ -10,3 +11,4 @@ app: Client = Client(name=session_name,api_id=info['api_id'],api_hash=info['api_
 app.start()
 app.stop()
 print(f'Сессия сохранена в {session_name}.session')
+input('Нажмите Enter чтобы выйти: ')
